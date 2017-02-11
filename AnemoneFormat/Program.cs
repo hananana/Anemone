a0fd352a610e063855d079574540d3d72413aa79
hananana <hananana.vimmer@gmail.com>
2017-02-12 01:49:52 +0900

    :hammer: mono!

--- AnemoneFormat/Program.cs
+++ AnemoneFormat/Program.cs
@@ -10,31 +10,36 @@ namespace CSharpFormat
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-			var argsOption = args [0];
-			var filePath = args [1];
+			var argsOption = args[0];
+			var filePath = args[1];
 			//recursive
-			if (argsOption == "-r") {
+			if (argsOption == "-r")
+			{
 				var files = GetCSharpFilePaths(filePath);
-				files.ForEach(path => {
+				files.ForEach(path =>
+				{
 					var result = FormattedString(path);
 					Overwrite(path, result);
 				});
 			}
             //single
-            else if (argsOption == "-s") {
-				var result = FormattedString (filePath);
-				Overwrite (filePath, result);
+            else if (argsOption == "-s")
+			{
+				var result = FormattedString(filePath);
+				Overwrite(filePath, result);
 			}
             //stdout
-            else if (argsOption == "-o") {
-				var result = FormattedString (filePath);
-				Console.WriteLine (result);
+            else if (argsOption == "-o")
+			{
+				var result = FormattedString(filePath);
+				Console.WriteLine(result);
 			}
             //from data
-            else if (argsOption == "-f") {
-				var str = args [1];
+            else if (argsOption == "-f")
+			{
+				var str = args[1];
 				var result = FormattedString(str);
 				Console.WriteLine(result);
 			}
@@ -42,8 +47,8 @@ namespace CSharpFormat
 
 		static List<string> GetCSharpFilePaths(string filePath)
 		{
-			var files = new List<string> ();
-			var dirs = new List<string> (Directory.GetDirectories(filePath));
+			var files = new List<string>();
+			var dirs = new List<string>(Directory.GetDirectories(filePath));
 			dirs.ForEach(dir => files.AddRange(Directory.GetFiles(dir, "*.cs")));
 			return files;
 		}
@@ -56,10 +61,9 @@ namespace CSharpFormat
 		static string FormattedString(string filePath)
 		{
 			var readed = File.ReadAllText(filePath);
-			var option = FormattingOptionsFactory.CreateAllman();
-            option.IndentBlocksInsideExpressions = false;
-			var textEditorOption = new TextEditorOptions ();
-			var formatter = new CSharpFormatter (option, textEditorOption);
+			var option = FormattingOptionsFactory.CreateMono();
+			var textEditorOption = new TextEditorOptions();
+			var formatter = new CSharpFormatter(option, textEditorOption);
 			return formatter.Format(readed);
 		}
 	}
